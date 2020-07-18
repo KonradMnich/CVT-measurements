57ada05959ef814f5236677ca9aec401636353c3
Konrad <67673285+KonradMnich@users.noreply.github.com>
2020-07-18 15:11:00 +0200

    Add save output feature

--- choice_of_velocity_sensor/step_3.py
+++ choice_of_velocity_sensor/step_3.py
@@ -15,14 +15,20 @@ import numpy as np
 path_in = "step_2_out/"
 path_out = "step_3_out/"
 
-x = pd.read_csv(path_in+"all_x_data.csv")
-x.drop(['l1'],axis=1,inplace=True)
+x_in = pd.read_csv(path_in+"all_x_data.csv")
+x_in.drop(['l1'],axis=1,inplace=True)
 
+x = pd.DataFrame()
 v = pd.DataFrame()
 a = pd.DataFrame()
 for i in range(9):
-    col = x.iloc[1:,i].astype(float).to_numpy()
+    col = x_in.iloc[1:,i].astype(float).to_numpy()
+    x[str(i)] = col[1:]
     dxdt = np.diff(col)
     v[str(i)] = dxdt
     ddxddt = np.diff(dxdt)
     a[str(i)] = ddxddt
+
+x.to_csv(path_out+'x.csv')
+v.to_csv(path_out+'v.csv')
+a.to_csv(path_out+'a.csv')

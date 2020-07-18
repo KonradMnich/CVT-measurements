e545909bb8addd3e5945ebf6a24b15846913c00c
Konrad <67673285+KonradMnich@users.noreply.github.com>
2020-07-18 15:06:45 +0200

    Add files

--- choice_of_velocity_sensor/step_3.py
+++ choice_of_velocity_sensor/step_3.py
@@ -9,5 +9,20 @@ This script serves to compute velocity and acceleration
 basing on displacement time series
 
 """
+import pandas as pd
+import numpy as np
+#first try - simple difference
+path_in = "step_2_out/"
+path_out = "step_3_out/"
 
+x = pd.read_csv(path_in+"all_x_data.csv")
+x.drop(['l1'],axis=1,inplace=True)
 
+v = pd.DataFrame()
+a = pd.DataFrame()
+for i in range(9):
+    col = x.iloc[1:,i].astype(float).to_numpy()
+    dxdt = np.diff(col)
+    v[str(i)] = dxdt
+    ddxddt = np.diff(dxdt)
+    a[str(i)] = ddxddt

0390415fcaf7208bd50d23cc22c8173bd30796e5
Konrad <67673285+KonradMnich@users.noreply.github.com>
2020-07-21 13:14:28 +0200

    Update files

--- 2-exemplary_measurement_treatment/step_1_data_preparation.py
+++ 2-exemplary_measurement_treatment/step_1_data_preparation.py
@@ -8,9 +8,17 @@ Created on Tue Jul 21 11:50:51 2020
 
 from reusable import DataConditioner
 
+# names of the files available for analysis
+file_names = [("r0v0a0mr.csv","r0v0a0pot.csv"),\
+              ("r0a1mr.csv","r0a1pot.csv"),\
+              ("r0a1bmr","r0a1bpot")]
+
+# create the object performing computations
 dc = DataConditioner("step_1_in/", "step_1_out/")
-dc.read_data_mr("r0a1bmr.csv")
-dc.read_data_ni("r0a1bpot.csv")
+dc.read_data_mr(file_names[0][0])
+dc.read_data_ni(file_names[0][1])
 dc.condition()
-dc.df_ready.plot(subplots=True)
+#%%
+# preview and save preprocessed data
+dc.df_ready[['F [N]','x [m]','v [m/s]']].plot(subplots=True)
 dc.save()

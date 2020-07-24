8a4c86ee38b79c0cc5db743ce00821a724186d88
Konrad <67673285+KonradMnich@users.noreply.github.com>
2020-07-24 21:32:29 +0200

    Add methods to preprocessor

--- 3-automatized_procedure/preprocessor.py
+++ 3-automatized_procedure/preprocessor.py
@@ -7,6 +7,7 @@ Created on Fri Jul 24 16:41:37 2020
 """
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 class Preprocessor:
     def __init__(self, list_of_inputs="list_of_inputs.csv", ratios=[0,16,32],\
@@ -19,6 +20,8 @@ class Preprocessor:
         self.raw_mr = self.read_mr()
         self.scaled_ni = self.scale_ni()
         self.scaled_mr = self.scale_and_expand_mr()
+        self.resample_ni()
+        self.synchronize_and_append()
         
     def read_ni(self):
         l=[]
@@ -46,7 +49,8 @@ class Preprocessor:
             # force
             F0 = (max(df['F'])+min(df['F']))/2
             df['F'] -= F0 # shifting zero
-            df['F'] *= 200  # scaling to Newtons
+            df['F'] *= -200  # scaling to Newtons
+            df['F'] = df['F'].rolling(10,center=True).mean().fillna(0)
             
             # displacement
             x0 = np.mean(df['x'].iloc[:10])
@@ -60,7 +64,7 @@ class Preprocessor:
         l=[]
         for n in self.raw_mr:
             df = n[0]
-            df['v'] *= 20/60/1000  # scaling rpm to m/s
+            df['v'] *= -20/60/1000  # scaling rpm to m/s
             #df['x'] = [0, *np.cumsum(df['v'])*n[1]]
             df['x'] = np.cumsum(df['v'])*n[1]
             df['a'] = [0, *np.diff(df['v'])/n[1]]
@@ -68,5 +72,38 @@ class Preprocessor:
             l += [[df,n[1]]]
         return l
     
-  
+    def resample_ni(self):
+        for i in range(len(self.scaled_ni)):
+            df = self.scaled_ni[i][0]
+            T_old = self.scaled_ni[i][1]
+            T_new = self.scaled_mr[i][1]
+            t_old = np.cumsum([T_old]*len(df))-T_old   
+            t_new = np.cumsum([T_new]*len(self.scaled_mr[i][0]))-T_new
+            df_new = pd.DataFrame()
+            df_new['x'] = np.interp(t_new,t_old,df['x'])
+            df_new['F'] = np.interp(t_new,t_old,df['F'])
+            self.scaled_ni[i][0] = df_new
+            
+    
+    def synchronize_and_append(self):
+        for i in range(len(self.df_in)):
+            x_mr =  self.scaled_mr[i][0]['x'].to_numpy()
+            x_ni =  self.scaled_ni[i][0]['x'].to_numpy()
+            se = sum((x_mr[200:-200]-x_ni[200:-200])**2)
+            for j in range(len(x_ni)):
+                x_ni = np.append(x_ni,x_ni[0])
+                x_ni = x_ni[1:]
+                se_temp = sum((x_mr[200:-200]-x_ni[200:-200])**2)
+                if se_temp < se:
+                    se = se_temp
+                    ind = j
+            F_ni =  self.scaled_ni[i][0]['F'].tolist()
+            self.scaled_mr[i][0]['F'] = F_ni[ind:] + F_ni[:ind]
+            ''' for debugging purpose
+            plt.figure()
+            plt.plot(x_mr,label='mr')
+            plt.plot(x_ni,label='before')
+            plt.plot(np.concatenate((x_ni[-ind:], x_ni[:ind])))
+            plt.legend(['mr','before','after'])
+            '''
             

fccd10ef22aed022519e1c80c39326431fd4fbc3
Konrad <67673285+KonradMnich@users.noreply.github.com>
2020-08-17 13:44:05 +0200

    Udpate to conform with pep8.

--- 3-automatized_procedure/processor.py
+++ 3-automatized_procedure/processor.py
@@ -13,6 +13,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class Processor:
+    """ Class used to identify physical parameters of the system
+    basing on the linear regression. """
+    
     def __init__(self, df):
         self.df = df
         self.customize_data()
@@ -21,32 +24,41 @@ class Processor:
         self.last_coeffs = pd.DataFrame()
     
     def customize_data(self):
-        # signum column replaces is for dry friction sign
+        """ Infere two new columns potentially usefull in regression."""
+        
+        # Signum column is used for dry friction sign.
         self.df['v_sign'] = self.df['v'].apply(np.sign)
-        # preliminary analysis has shown that the force is proportional
-        # to a root of velocity rather than velocity itself
+        # Preliminary analysis has shown that the force is proportional
+        # to a power of velocity rather than velocity itself.
         self.df['v**'] = self.df['v'].apply(abs)**1*self.df['v_sign']
     
     def train(self, pos_v=[True,False], pos_a=[True,False], t_size=0.4):
-        self.df_temp = self.df[(self.df['v']>0).isin(pos_v) &\
+        """ This function computes linear regression basing on data
+        from preprocessor. """
+        
+        self.df_temp = self.df[(self.df['v']>0).isin(pos_v) &
                                (self.df['a']>0).isin(pos_a)]
-        # measurement method bugs the results when system is not in motion
+        # Measurement method bugs the results when system is not in motion
+        # and therefor we exclude zero velocities form the model.
         self.df_temp = self.df_temp[self.df_temp['v'] != 0]
-        X = self.df_temp[['v_sign','v**','a']]
+        X = self.df_temp[['v_sign', 'v**', 'a']]
         y = self.df_temp['F']
         X_train, X_test, y_train, y_test =\
             train_test_split(X, y, test_size=t_size)
-        self.lm.fit(X_train,y_train)
-        self.last_coeffs = pd.DataFrame(self.lm.coef_,['dry','viscous','inertance']\
-                                        ,columns=['Coefficient'])
-        #self.last_coeffs.loc['free term'] = [self.lm.intercept_]
+        self.lm.fit(X_train, y_train)
+        self.last_coeffs = pd.DataFrame(self.lm.coef_,
+                                        ['dry','viscous','inertance'],
+                                        columns=['Coefficient'])
         print(self.last_coeffs)
         
         predictions = self.lm.predict(X_test)
+        # Plot the errors histogram and correlation plot to asses
+        # the quality of fit.
         plt.figure()
         sns.distplot((y_test-predictions),bins=50);
         plt.figure()
         sns.scatterplot(x=predictions,y=y_test)
         
     def out(self):
+        """ Function added for consistency."""
         return self.last_coeffs
